d98bc11315d934fc9b3cb2c8eb48c0ff94550020
Fernando Lafuente Saiz <>
2022-04-06 01:05:12 +0200

    Throw error for status 200 errors

--- src/api/candidates.tsx
+++ src/api/candidates.tsx
@@ -3,6 +3,7 @@ import { Candidate } from '@slices/candidates';
 
 type CandidateResponse = {
   data: Array<Candidate>;
+  error: boolean;
 };
 
 const staggeredBaseQuery = retry(fetchBaseQuery({ baseUrl: 'http://personio-fe-test.herokuapp.com/api/v1/' }), {
@@ -15,9 +16,12 @@ export const candidatesApi = createApi({
   endpoints: (builder) => ({
     getCandidatesList: builder.query({
       query: () => `candidates`,
-      transformResponse: (response:CandidateResponse) => response?.data.sort((a:Candidate, b:Candidate) => {
-        return b.application_date.localeCompare(a.application_date); 
-      }),
+      transformResponse: (response:CandidateResponse) => {
+        if(response.error) throw response.error;
+        return response?.data.sort((a:Candidate, b:Candidate) => {
+          return b.application_date.localeCompare(a.application_date); 
+        })
+      },
     }),
   }),
 });
