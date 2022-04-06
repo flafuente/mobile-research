ff3a5c5dfc4889649b8e4bdc5954eee9c748d86b
Fernando Lafuente Saiz <>
2022-04-06 02:59:22 +0200

    Dynamic Detail screen title

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStackNavigator, StackScreenProps } from "@react-navigation/stack";
+import { StackHeaderOptions } from "@react-navigation/stack/lib/typescript/src/types";
 import { Home, Detail } from '@screens/index'
 import { Candidate } from '@slices/candidates'
 
@@ -11,12 +12,18 @@ type RootStackParamList = {
 export type Props = StackScreenProps<RootStackParamList, 'Detail'>;
 
 const Stack = createStackNavigator();
-
+const header = ({ route }: Props) => ({
+  title: route.params?.name ?? ''
+}) 
 const MainStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Detail" component={Detail} />
+      <Stack.Screen name="Candidates" component={Home} />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={header as StackHeaderOptions}
+      />
     </Stack.Navigator>
   );
 };
